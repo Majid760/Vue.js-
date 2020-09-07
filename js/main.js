54a764bdf48f8cf87a0e275fcc191c10de083ecc
Majid760 <55375676+Majid760@users.noreply.github.com>
2020-09-08 03:07:44 +0500

    Vue.js project who pay the bills

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const app=new Vue({
+  el:'#app',
+  data:{
+    state:true,
+    inputName:'',
+    names:[],
+    error:false,
+    result:'',
+  },
+  methods:{
+    addNameToList(){
+      if(this.validate(this.inputName)){
+        this.names.push(this.inputName);
+        this.inputName='';
+        this.error=false;
+      }else{
+          this.error=true;
+      }
+    },
+    validate(value){
+      if(value!==''){
+        return true;
+      }else{
+        return false;
+      }
+    },
+    showResult(){
+      let rand=this.names[Math.floor(Math.random()*this.names.length)];
+      this.result=rand;
+      this.state=false;
+    },
+    resetApp(){
+      this.state=true;
+      this.names=[];
+      this.result='';
+    },
+    removeName(index){
+      this.names.splice(index,1);
+    }
+  }
+})
